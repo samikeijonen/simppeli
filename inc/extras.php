55074ca4769b5fed7f71db5604c677d6be3d9c96
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-06-15 22:26:24 +0300

    Remove comment.

--- inc/extras.php
+++ inc/extras.php
@@ -2,8 +2,6 @@
 /**
  * Custom functions that act independently of the theme templates
  *
- * Eventually, some of the functionality here could be replaced by core features
- *
  * @package Simppeli
  */
 
