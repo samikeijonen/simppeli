155f8f3b4c571af05410d16138403cdcbc84e0a3
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-06-15 17:10:47 +0300

    Use theme version number.

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'simppeli_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function simppeli_customize_preview_js() {
-	wp_enqueue_script( 'simppeli_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'simppeli_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), SIMPPELI_VERSION, true );
 }
 add_action( 'customize_preview_init', 'simppeli_customize_preview_js' );
