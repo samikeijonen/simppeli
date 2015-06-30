c3c088abd54e77e65ba410a15aeb549873d97979
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-06-30 21:46:28 +0300

    Add comments wrapper.

--- comments.php
+++ comments.php
@@ -18,62 +18,66 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments-wrapper" class="comments-area-wrapper">
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'simppeli' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2>
+	<div id="comments" class="comments-area">
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'simppeli' ); ?></h2>
-			<div class="nav-links">
+		<?php if ( have_comments() ) : ?>
+			<h2 class="comments-title">
+				<?php
+					printf( // WPCS: XSS OK.
+						esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'simppeli' ) ),
+						number_format_i18n( get_comments_number() ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				?>
+			</h2>
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'simppeli' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'simppeli' ) ); ?></div>
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+			<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
+				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'simppeli' ); ?></h2>
+				<div class="nav-links">
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
+					<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'simppeli' ) ); ?></div>
+					<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'simppeli' ) ); ?></div>
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 56,
-				) );
-			?>
-		</ol><!-- .comment-list -->
+				</div><!-- .nav-links -->
+			</nav><!-- #comment-nav-above -->
+			<?php endif; // Check for comment navigation. ?>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'simppeli' ); ?></h2>
-			<div class="nav-links">
+			<ol class="comment-list">
+				<?php
+					wp_list_comments( array(
+						'style'       => 'ol',
+						'short_ping'  => true,
+						'avatar_size' => 56,
+					) );
+				?>
+			</ol><!-- .comment-list -->
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'simppeli' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'simppeli' ) ); ?></div>
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+			<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'simppeli' ); ?></h2>
+				<div class="nav-links">
 
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-below -->
-		<?php endif; // Check for comment navigation. ?>
+					<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'simppeli' ) ); ?></div>
+					<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'simppeli' ) ); ?></div>
 
-	<?php endif; // Check for have_comments(). ?>
+				</div><!-- .nav-links -->
+			</nav><!-- #comment-nav-below -->
+			<?php endif; // Check for comment navigation. ?>
 
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'simppeli' ); ?></p>
-	<?php endif; ?>
+		<?php endif; // Check for have_comments(). ?>
 
-	<?php comment_form(); ?>
+		<?php
+			// If comments are closed and there are comments, let's leave a little note, shall we?
+			if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		?>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'simppeli' ); ?></p>
+		<?php endif; ?>
 
-</div><!-- #comments -->
+		<?php comment_form(); ?>
+
+	</div><!-- #comments -->
+
+</div><!-- #comments-wrapper -->
