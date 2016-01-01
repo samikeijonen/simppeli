d58644324605f9e9e4e11b044edd3a4c5d780c94
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-01-01 12:45:14 +0200

    Escape Pages string for better security.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -20,11 +20,11 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'simppeli' ) . '</span>',
+				'before'      => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'simppeli' ) . '</span>',
 				'after'       => '</div>',
 				'link_before' => '<span>',
 				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'simppeli' ) . ' </span>%',
+				'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'simppeli' ) . ' </span>%',
 				'separator'   => '<span class="screen-reader-text">, </span>',
 			) );
 		?>
