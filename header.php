bb7dfe89f9cd27b393bab8860c3ca0fb2af8c060
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-01-01 12:31:55 +0200

    Use H1 only in blog index on Site Title.

--- header.php
+++ header.php
@@ -25,8 +25,20 @@
 	<header id="masthead" class="site-header" role="banner">
 	
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<div class="site-description"><?php bloginfo( 'description' ); ?></div>
+		
+			<?php
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
+			
 		</div><!-- .site-branding -->
 
 		<?php get_template_part( 'menu', 'primary' ); // Loads the menu-primary.php template. ?>
