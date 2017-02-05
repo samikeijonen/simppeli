a380fa0c4b45fc942fd90950c7564a34695d3631
Sami Keijonen <sami.keijonen@foxnet.fi>
2017-02-05 14:46:44 +0200

    Remove rel="designer" from the footer.

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'simppeli' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'simppeli' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'simppeli' ), 'Simppeli', '<a href="http://foxland.fi/" rel="designer">Foxland</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'simppeli' ), 'Simppeli', '<a href="http://foxland.fi/">Foxland</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
